f5797c79b46f56ef0cf5746537362c38673ad514
marbeelz <45430712+marbeelz@users.noreply.github.com>
2019-08-08 14:25:43 +0700

    update Recycler baru

---
+++ app/src/main/java/com/example/marbeelz/ptdutamancagraha/DetailFragment.java
@@ -0,0 +1,31 @@
+package com.example.marbeelz.ptdutamancagraha;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class DetailFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_detail, container, false);
+
+    }
+}
